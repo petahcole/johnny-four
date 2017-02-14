0193688716e72bc088f1cd085e183a8906148635
Andy Herzog <asherzog19@gmail.com>
2017-02-14 14:03:28 -0700

    test2

--- public/app/drive-view/drive-view.controller.js
+++ public/app/drive-view/drive-view.controller.js
@@ -10,6 +10,17 @@
         const vm = this;
         vm.$onInit = function() {
             console.log('drive me ooooh')
+
+            var canvas = document.createElement("canvas");
+            document.body.appendChild(canvas);
+
+            // Create h264 player
+            var uri = "ws://" + document.location.host;
+            var wsavc = new WSAvcPlayer(canvas, "webgl", 1, 35);
+            wsavc.connect(uri);
+
+            //expose instance for button callbacks
+            window.wsavc = wsavc;
         }
 
     }
