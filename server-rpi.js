7575a391f1da5c111d0ecf0299dc25ff31c2258b
Murph Grainger <murph.grainger@gmail.com>
2017-02-14 13:32:17 -0700

    added script to index

--- server-rpi.js
+++ server-rpi.js
@@ -22,4 +22,4 @@ app.use(express.static(__dirname + '/vendor/dist'));
 const server = http.createServer(app);
 const silence = new WebStreamerServer(server);
 
-server.listen(8080);
+server.listen(1234);
