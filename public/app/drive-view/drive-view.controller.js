1ddb2e20476424ac408fabddc7e0171e89daf246
Murph Grainger <murph.grainger@gmail.com>
2017-02-14 12:44:17 -0700

    initial commit

--- public/app/drive-view/drive-view.controller.js
+++ public/app/drive-view/drive-view.controller.js
@@ -10,14 +10,6 @@
         const vm = this;
         vm.$onInit = function() {
             console.log('drive me ooooh')
-
-            // Create h264 player
-            vm.uri = "ws://" + document.location.host;
-            vm.wsavc = new WSAvcPlayer(vm.stream, "webgl", 1, 35);
-            vm.wsavc.connect(vm.uri);
-
-            //expose instance for button callbacks
-            window.wsavc = vm.wsavc;
         }
 
     }
